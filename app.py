7a0b44ae5e7bae40061079c5bb2da4bec3d7ac51
PanBazant <PanBazant@users.noreply.github.com>
2022-06-16 01:56:00 +0200

    add reservation form handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, json
-from itsdangerous import encoding
+from flask import Flask, render_template, url_for, json, request
+import json
 
 
 app = Flask(__name__)
@@ -18,10 +18,21 @@ def menu():
     return render_template("menu.html", title="page", jsonfile=json.dumps(data))
 
 
-@app.route("/reservation")
+@app.route("/reservation", methods=["GET", "POST"])
 def reservation():
-    return render_template("reservation.html")
+    if request.method == "GET":
+        return render_template("reservation.html")
+    else:
+        
+        # for i in request.form:
+        #     print("{} {}".format(request.form[i], i))
 
+        json_object = json.dumps(request.form, indent = 4)
+
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+
+        return render_template("reservation_post.html")
 
 @app.route("/contact")
 def contact():
